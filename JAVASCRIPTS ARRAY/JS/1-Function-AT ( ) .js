a7042e5abc1f9468e1588e5a4a7f3612ed26f406
Ing-Javier-Sistem <145406699+Ing-Javier-Sistem@users.noreply.github.com>
2024-02-24 18:36:09 -0500

    nuevo Repo

---
+++ JAVASCRIPTS ARRAY/JS/1-Function-AT ( ) .js
@@ -0,0 +1,8 @@
+function Array_at(num) {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    array.at(num);
+    // LA FUNCION DE JS devuelve el valor en la posición indicada del array
+}
+
+const llamar = Array_at(2);
+console.log(llamar)
---
+++ JAVASCRIPTS ARRAY/JS/2-Function-PUSH ( ) .js
@@ -0,0 +1,12 @@
+function Array_push(num, texto){
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    array.push(num);
+    array.push(texto);
+    // LA FUNCION DE JS agrega valores al array
+
+    return array; 
+}
+
+const llamar = Array_push(10, 'Hola Mundo!');
+console.log(llamar);
