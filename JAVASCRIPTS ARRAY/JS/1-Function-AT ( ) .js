67de2e1fe18d3ab674043de2ddf07b3860632fe7
Ing-Javier-Sistem <145406699+Ing-Javier-Sistem@users.noreply.github.com>
2024-02-24 20:51:02 -0500

    3.0

--- JAVASCRIPTS ARRAY/JS/1-Function-AT ( ) .js
+++ JAVASCRIPTS ARRAY/JS/1-Function-AT ( ) .js
@@ -1,7 +1,7 @@
 function Array_at(num) {
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     array.at(num);
-    
+    // LA FUNCION DE JS devuelve el valor en la posición indicada del array
 }
 
 const llamar = Array_at(2);
